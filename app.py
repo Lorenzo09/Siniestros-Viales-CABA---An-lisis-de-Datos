40da2b64bfa9ef15394811a2bf10e0d0121f6e17
Lorenzo Lacava <lorenzolacava1@gmail.com>
2024-06-04 20:55:10 -0300

    Se agrega el readme, y los cambios finales al proyecto

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import pandas as pd
-import matplotlib as mp
+import matplotlib.pyplot as plt
 import seaborn as sb
 import streamlit as st
 import calendar
+import folium
+from streamlit_folium import folium_static
+from folium.plugins import MarkerCluster
+
+# Configuración inicial para seaborn
+sb.set(style="whitegrid")
+
+# Colores para los gráficos
+COLOR_PALETTE = ['#3498db', '#2ecc71', '#e74c3c', '#9b59b6', '#f1c40f', '#34495e']
 
 # Cargar los archivos CSV
 @st.cache_data
@@ -11,28 +20,76 @@ def load_data(file_path):
 
 # Definir los paths de los archivos CSV junto con sus nombres
 file_paths = {
-    "Datasets\df_siniestros_viales_streamlit.csv": "Siniestros Viales CABA",
-    "Datasets\df_siniestros_anios.csv": "Siniestros Viales CABA por año y mes",
-    "Datasets\df_siniestros_comunas.csv": "Siniestros Viales CABA por Comuna"
+    "Datasets/df_siniestros_viales_streamlit.csv": "Siniestros Viales CABA",
+    "Datasets/df_siniestros_anios.csv": "Siniestros Viales CABA por Año y Mes",
+    "Datasets/df_siniestros_comunas.csv": "Siniestros Viales CABA por Comuna"
 }
 
 # Función para cargar y mostrar dataframes
 def show_dataframes():
-    st.title('Datos utilizados para el Dashboard')
+    st.title('📊 Datos utilizados para el Dashboard')
     # Cargar cada archivo CSV y mostrar sus datos
     for file_path, name in file_paths.items():
         st.write(f"## {name}")
         data = load_data(file_path)
         st.write(data)
 
+# Función para mostrar los KPIs
+def show_kpis():
+    st.title('📈 Indicadores Clave de Desempeño (KPIs)')
+
+    # KPI 1: Tasa de homicidios
+    st.subheader('🔴 Tasa de Homicidios - Primer y Segundo Semestre 2021')
+    tasa_primer_semestre_2021 = 1.761857
+    tasa_segundo_semestre_2021 = 1.345418
+    reduccion_10 = tasa_primer_semestre_2021 * 0.9
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.bar(['Primer Semestre 2021', 'Segundo Semestre 2021'], [tasa_primer_semestre_2021, tasa_segundo_semestre_2021], color=COLOR_PALETTE[:2])
+    ax.axhline(reduccion_10, color='red', linestyle='--', label='10% de reducción')
+    ax.set_xlabel('Semestre')
+    ax.set_ylabel('Tasa de homicidios')
+    ax.set_title('Comparación de tasas de homicidios - Primer y Segundo Semestre 2021')
+    ax.legend()
+    ax.grid(True)
+    st.pyplot(fig)
+
+    # KPI 2: Accidentes de motos
+    st.subheader('🛵 Cantidad de accidentes mortales de motociclistas - 2020 y 2021')
+    siniestros_moto = load_data("Datasets/df_siniestros_viales_streamlit.csv")
+    siniestros_moto = siniestros_moto[siniestros_moto['VICTIMA'] == 'MOTO']
+
+    # Convertir la columna de fecha a tipo datetime
+    siniestros_moto['FECHA'] = pd.to_datetime(siniestros_moto['FECHA'])
+
+    # Extraer el año de la columna de fecha
+    siniestros_moto['AÑO'] = siniestros_moto['FECHA'].dt.year
+
+    accidentes_2020 = siniestros_moto[siniestros_moto['AÑO'] == 2020]['N_VICTIMAS'].sum()
+    accidentes_2021 = siniestros_moto[siniestros_moto['AÑO'] == 2021]['N_VICTIMAS'].sum()
+    porcentaje_reduccion = ((accidentes_2020 - accidentes_2021) / accidentes_2020) * 100
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.bar(['2020', '2021'], [accidentes_2020, accidentes_2021], color=COLOR_PALETTE[2:4])
+    ax.set_title('Cantidad de accidentes mortales de motociclistas')
+    ax.set_xlabel('Año')
+    ax.set_ylabel('Cantidad de accidentes mortales')
+    ax.text(0, accidentes_2020 + 10, f'{accidentes_2020}', ha='center', color='blue')
+    ax.text(1, accidentes_2021 + 10, f'{accidentes_2021}', ha='center', color='orange')
+    ax.set_ylim(0, max(accidentes_2020, accidentes_2021) * 1.2)
+    ax.grid(axis='y', linestyle='--', alpha=0.7)
+    ax.text(0.5, (accidentes_2020 + accidentes_2021) / 2, f'Reducción del {porcentaje_reduccion:.2f}%', ha='center', va='center', fontsize=12, color='red')
+    st.pyplot(fig)
+
 # Función para generar el dashboard
 def show_dashboard():
-    st.title('Siniestros Viales en CABA durante 2016 y 2021')
+    st.title('🚦 Siniestros Viales en CABA durante 2016 y 2021')
     # Definir el path del archivo CSV
-    siniestros_viales = "Datasets\df_siniestros_viales_streamlit.csv"
+    siniestros_viales = "Datasets/df_siniestros_viales_streamlit.csv"
 
     # Cargar los datos
     data = load_data(siniestros_viales)
+    data_comunas = load_data("Datasets/df_siniestros_comunas.csv")
 
     # Convertir la columna de fecha a tipo datetime
     data['FECHA'] = pd.to_datetime(data['FECHA'])
@@ -47,71 +104,143 @@ def show_dashboard():
     # Agrupar por año y mes y contar la cantidad de siniestros
     siniestros_por_mes = data.groupby(['AÑO', 'MES'])['N_VICTIMAS'].sum().reset_index()
 
-    # Crear el gráfico interactivo    
-    year_to_show = st.selectbox('Selecciona el año:', sorted(siniestros_por_mes['AÑO'].unique()))
-    selected_data = siniestros_por_mes[siniestros_por_mes['AÑO'] == year_to_show]
-    st.title(f"Cantidad de siniestros viales por mes en el año {year_to_show}")
+    # Inicializar selected_view en st.session_state si no está definido
+    if 'selected_view' not in st.session_state:
+        st.session_state.selected_view = 'mes'
 
-    # Filtrar los datos seleccionados por año
-    selected_data = selected_data.set_index('MES').loc[:, ['N_VICTIMAS']]
+    selected_view = st.session_state.selected_view
 
-    # Graficar los datos como un gráfico de barras
-    st.bar_chart(selected_data)
+    # Gráficos
+    if selected_view == "mes":
+        col1, col2 = st.columns(2)
 
-    # Agrupar por año y mes y contar la cantidad de siniestros
-    siniestros_por_mes = data.groupby(['AÑO', 'MES'])['N_VICTIMAS'].sum().reset_index()
+        # Columna 1: Gráfico de barras por mes
+        with col1:
+            # Gráfico de siniestros por mes
+            year_to_show = st.selectbox('Selecciona el año:', sorted(siniestros_por_mes['AÑO'].unique()))
+            selected_data = siniestros_por_mes[siniestros_por_mes['AÑO'] == year_to_show]
+            st.title("")
+            selected_data = selected_data.set_index('MES').loc[:, ['N_VICTIMAS']]
+            st.bar_chart(selected_data)
 
-    # Convertir el nombre del mes a número
-    siniestros_por_mes['MES_NUMERO'] = siniestros_por_mes['MES'].apply(lambda x: list(calendar.month_name).index(x))
+        # Columna 2: Gráfico de torta por comuna
+        with col2:
+            st.title("Porcentaje de Víctimas por Comuna")
+            # Filtrar los datos por el año seleccionado
+            comunas_data = data_comunas[data_comunas['Año'] == year_to_show]
 
-    # Graficar la cantidad de siniestros viales por año y mes
-    st.title("Cantidad de siniestros viales por año y mes")
+            # Agrupar por comuna y sumar las víctimas
+            comunas_data = comunas_data.groupby('COMUNA')['N_VICTIMAS'].sum().reset_index()
 
-    # Seleccionar los años que el usuario quiere ver
-    years_to_show = st.multiselect('Selecciona los años:', sorted(siniestros_por_mes['AÑO'].unique()))
+            # Ordenar por número de víctimas y seleccionar las 3 comunas con más víctimas
+            comunas_data = comunas_data.sort_values(by='N_VICTIMAS', ascending=False)
+            top_3_comunas = comunas_data[:3]
+            other_comunas = pd.DataFrame([{
+                'COMUNA': 'Otras',
+                'N_VICTIMAS': comunas_data[3:]['N_VICTIMAS'].sum()
+            }])
 
-    # Filtrar los datos según los años seleccionados
-    filtered_data = siniestros_por_mes[siniestros_por_mes['AÑO'].isin(years_to_show)]
+            # Concatenar las 3 comunas principales con la categoría "Otras"
+            final_comunas_data = pd.concat([top_3_comunas, other_comunas])
 
-    # Crear el gráfico de líneas
-    chart_data = filtered_data.pivot(index='MES_NUMERO', columns='AÑO', values='N_VICTIMAS')
-    st.line_chart(chart_data, use_container_width=True)
+            # Crear el gráfico de torta
+            fig, ax = plt.subplots()
+            ax.pie(final_comunas_data['N_VICTIMAS'], labels=final_comunas_data['COMUNA'], autopct='%1.1f%%', startangle=90, colors=COLOR_PALETTE)
+            ax.axis('equal')  # Para asegurar que el gráfico sea un círculo.
+            st.pyplot(fig)
 
-                        #Nuevo Gráfico
-    import folium
-    from streamlit_folium import folium_static
-    from folium.plugins import MarkerCluster
+    elif selected_view == "año_mes":
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            # Gráfico de siniestros por año y mes
+            st.title("📅 Cantidad de siniestros viales por año y mes")
+            years_to_show = st.multiselect('Selecciona los años:', sorted(siniestros_por_mes['AÑO'].unique()), default=[2016], key='multiselect_years')
+            siniestros_por_mes['MES_NUMERO'] = siniestros_por_mes['MES'].apply(lambda x: list(calendar.month_name).index(x))
+            filtered_data = siniestros_por_mes[siniestros_por_mes['AÑO'].isin(years_to_show)]
+            chart_data = filtered_data.pivot(index='MES_NUMERO', columns='AÑO', values='N_VICTIMAS')
+            chart_data.index = [calendar.month_name[i] for i in chart_data.index]  # Convertir los números de los meses a nombres de meses
+            st.line_chart(chart_data)
 
-    st.title("Mapa de siniestros viales en CABA")
+        with col2:
+            # Gráfico de siniestros por tipo de víctima y mes
+            st.title("🚶‍♂️🚗 Siniestros por Tipo de Víctima y Mes")
 
-    # Crear un mapa centrado en CABA
-    mapa = folium.Map(location=[-34.6083, -58.3712], zoom_start=12)
+            # Agregar selección de año y mes
+            selected_year = st.selectbox('Selecciona el año:', sorted(data['AÑO'].unique()), key='select_year_victims')
+            selected_month = st.selectbox('Selecciona el mes:', list(calendar.month_name)[1:], key='select_month_victims')
 
-    # Agrupar los datos por comuna
-    grouped_data = data.groupby('COMUNA')
+            # Filtrar los datos según el año y mes seleccionados
+            filtered_data = data[(data['AÑO'] == selected_year) & (data['MES'] == selected_month)]
 
-    # Crear un grupo de marcadores
-    marker_cluster = folium.plugins.MarkerCluster().add_to(mapa)
+            # Agrupar los datos filtrados por tipo de víctima
+            victim_data = filtered_data.groupby('VICTIMA')['N_VICTIMAS'].sum().reset_index()
 
-    # Iterar sobre cada fila de los datos
-    for index, row in data.iterrows():
-        folium.Marker([row['pos y'], row['pos x']], popup=row['N_VICTIMAS']).add_to(marker_cluster)
+            fig, ax = plt.subplots(figsize=(10, 6))
+            sb.barplot(data=victim_data, x='VICTIMA', y='N_VICTIMAS', ax=ax)
+            ax.set_xlabel('Tipo de Víctima')
+            ax.set_ylabel('Cantidad de Siniestros')
+            ax.set_title(f'Siniestros por Tipo de Víctima en {selected_month} {selected_year}')
+            st.pyplot(fig)
 
-    # Mostrar el mapa
-    folium_static(mapa)
 
+    elif selected_view == "mapa":
+        col1, col2 = st.columns([3, 1])
+        
+        with col1:
+            # Mapa de siniestros
+            st.title("🗺️ Mapa de siniestros viales en CABA")
+            mapa = folium.Map(location=[-34.6083, -58.3712], zoom_start=12)
+            marker_cluster = MarkerCluster().add_to(mapa)
+            for index, row in data.iterrows():
+                folium.Marker([row['pos y'], row['pos x']], popup=row['N_VICTIMAS']).add_to(marker_cluster)
+            folium_static(mapa)
+        
+        with col2:
+            st.title("Mapa de Comunas")
+            st.image("Imagenes/comunas.jpg", use_column_width=True)
 
+# Configurar la barra lateral para la navegación entre páginas y gráficos
+st.sidebar.title("🗂️ Navegación")
 
-    
-# Configurar la barra lateral para la navegación entre páginas
 pages = {
     "Dashboard": show_dashboard,
-    "Dataframes": show_dataframes
+    "Dataframes": show_dataframes,
+    "KPIs": show_kpis
 }
-selection = st.sidebar.radio("Navegación", list(pages.keys()))
+selection = st.sidebar.radio("Elige una página:", list(pages.keys()))
+
+if selection == "Dashboard":
+    st.sidebar.title("📊 Menú de Gráficos")
+    if st.sidebar.button("Siniestros por Mes"):
+        st.session_state.selected_view = "mes"
+    if st.sidebar.button("Siniestros por Año y Mes"):
+        st.session_state.selected_view = "año_mes"
+    if st.sidebar.button("Mapa de Siniestros"):
+        st.session_state.selected_view = "mapa"
 
 # Ejecutar la función correspondiente a la página seleccionada
 pages[selection]()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ readme.md
@@ -0,0 +1,88 @@
+<h1 align='center'>
+ <b>Siniestros viales en la Ciudad de Buenos Aires 2016 - 2021</b>
+</h1>
+
+![image](Imagenes\readme1.jpg)
+
+<h2 align='center'>
+ <b> Segundo Proyecto Individual Henry</b>
+</h2>
+
+![image](Imagenes\readme2.jpg)
+
+La Ciudad Autónoma de Buenos Aires (CABA), capital de la República Argentina, es una metrópoli vibrante y llena de historia, considerada uno de los principales centros culturales y económicos de América Latina. Con una población de aproximadamente 3 millones de habitantes, Buenos Aires es un núcleo cosmopolita donde se fusionan la tradición y la modernidad, atrayendo a millones de turistas cada año. La ciudad se destaca por su arquitectura europea, sus teatros, y su vida nocturna, además de ser la cuna del tango, un género musical y baile reconocido internacionalmente.
+
+Históricamente, Buenos Aires ha sido el escenario de numerosos eventos significativos para la nación argentina. Fundada en 1580 por Juan de Garay, la ciudad ha crecido y evolucionado, desde sus raíces coloniales hasta convertirse en un centro urbano de relevancia global. Entre sus monumentos más emblemáticos se encuentran el Obelisco, el Teatro Colón y la Casa Rosada, sede del Poder Ejecutivo Nacional. Además, sus barrios como San Telmo, La Boca, y Recoleta ofrecen un vistazo a la rica herencia cultural y artística de la ciudad.
+
+Sin embargo, detrás de esta fachada vibrante y culturalmente rica, Buenos Aires enfrenta serios desafíos, uno de los más graves es la seguridad vial. Los siniestros viales representan un problema enorme para la ciudad, afectando tanto a los residentes como a los visitantes. Según datos recientes, los accidentes de tránsito en la ciudad han causado un alarmante número de víctimas y heridos, lo que subraya la necesidad de medidas urgentes y eficaces para mejorar la seguridad vial.
+
+Entre 2016 y 2021, Buenos Aires ha registrado una preocupante cantidad de siniestros viales, muchos de los cuales han resultado en fatalidades. Estos incidentes no solo causan un dolor inmenso a las familias afectadas, sino que también generan un impacto negativo en el sistema de salud pública y en la economía local. La gran cantidad de accidentes involucra a diversos tipos de vehículos, incluidos automóviles, motocicletas y peatones, lo que destaca la urgencia de implementar estrategias de prevención y educación vial.
+
+La situación de los siniestros viales en Buenos Aires requiere atención inmediata y la colaboración de todos los sectores de la sociedad. Las autoridades municipales están trabajando en diversas iniciativas para reducir estos números alarmantes, como la mejora de la infraestructura vial, la implementación de campañas de concientización y la promoción de normas de tránsito más estrictas. Solo con un esfuerzo conjunto será posible hacer de Buenos Aires una ciudad más segura para todos sus habitantes y visitantes.
+
+
+## Desarrollo del proyecto
+
+El primer paso fue desmenuzar los datos. Para ello, me centré en realizar un proceso de ETL (Extracción, Transformación y Carga) con el cual definí los valores necesarios para entender los datos de la problemática. Así, analicé la composición y el tamaño de los datasets disponible, con el propósito de observar similitudes, diferencias, datos atipicos.
+
+Para continuar, comencé un proceso de EDA (Análisis de Datos Exploratorio), para empezar a graficar ya con los datos correctamente cargados y entender el desafío al que se enfrenta la Ciudad Autónoma de Buenos Aires.
+
+![image](Imagenes\readme3.jpg.png)
+
+
+Todo el proceso se puede constatar en el link del repositorio (EDA.ipynb):
+
+Con estos primeros datos, podemos observar que existe un fuerte disminución en la cantidad de siniestros viales en el año 2020. Por lo tanto, decidí verificar que no se tratase de datos erróneos.
+
+![image](Imagenes\Casospordia.jpeg)
+
+La variación en el año 2020 puede ser constatada gracias a los datos oficiales del organismo de salud en la Ciudad Autónoma de Buenos Aires. Allí, se vivió una de las cuarentenas más estrictas del país, por lo cual se vió afectado el número de siniestros viales. Sin embargo, esto significa que los datos no son erróneos y podemos continuar.
+
+![image](Imagenes\readme4.jpg.png)
+
+## Primer KPI 
+
+Reducir en un 10% la tasa de homicidios en siniestros viales de los últimos seis meses en la Ciudad Autónoma de Buenos Aires (CABA), en comparación con la tasa del semestre anterior.teniendo en cuenta que el Sistema Nacional de Información Criminal (SNIC) define la tasa de homicidios en siniestros viales como el número de víctimas fatales en accidentes de tránsito por cada 100,000 habitantes en un área geográfica durante un período de tiempo específico. Su fórmula es: (Número de homicidios en siniestros viales / Población total) * 100,000.
+
+Con el siguiente gráfico, buscaba darle solución al primer KPI, el cual buscaba la diferencia entre los últimos semestres del año. En el cual podemos confirmar que hubo una reducción de siniestros viales. Para ello, podemos mirar el Dashboard interactivo.
+
+![image](Imagenes\readme5.jpeg)
+
+Para continuar, comencé a graficar los distintos siniestros por comuna y tipo de vehículo. Allí, se puede ver con claridad que las víctimas fatales más comunes son los que se encuentran manejando una moto.
+
+![image](Imagenes\readme6.png)
+
+## Segundo KPI 
+
+Reducir en un 7% la cantidad de accidentes mortales de motociclistas en el último año en la Ciudad Autónoma de Buenos Aires (CABA), en comparación con el año anterior.
+
+Para el siguiente KPI, habría que comparar ambos gráficos realizados. En los cuales podemos ver la cantidad de víctimas y la cantidad de víctimas motociclistas. Para ello, podemos también acceder al Dashboard.
+
+![image](Imagenes\readme7.jpeg)
+
+Con este KPI, podemos ver que la cantidad de víctimas lamentablemente no sólo ni disminuyó. Si no que aumentó considerablemente.
+
+## Tercer KPI 
+
+Para finalizar, en el tercer y último KPI, muestro cómo a pesar de la pandemia, el año 2021 tuvo una reducción en su cantidad de siniestros viales sobre autopistas frente al año 2020.
+
+![image](Imagenes\readme8.png)
+
+
+## Creacion del Dashboard
+
+Para finalizar el proyecto, decidí hacer un pequeño Dashboard con STREAMLIT. En él, utilicé los gráficos de manera eficiente para que cualquiera pueda visualizar los siniestros viales en CABA durante los años 2016 y 2021. Además de graficar los KPIs presentados en este documento.
+
+
+## Datos utilizados
+
+- [INDEC Instituto Nacional de Estadística de la República Argentina](https://www.indec.gob.ar/indec/web/Nivel4-Tema-2-41-165) <br>
+- [Buenos Aires Data](https://www.indec.gob.ar/indec/web/Nivel4-Tema-2-41-165)
+
+## Tecnologías utilizadas
+
+- [pandas](https://pandas.pydata.org/)
+- [Streamlit](https://streamlit.io/)
+
+## Contacto 
+- Correo electrónico: lorenzolacava1@gmail.com
